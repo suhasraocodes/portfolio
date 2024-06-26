685ac8ce631c8ff225deed7e17c15b52eb032d44
stranger-creator <raosuhas0@gmail.com>
2024-06-26 23:08:21 +0530

    cleaning-up

--- JS/main.js
+++ JS/main.js
@@ -56,3 +56,14 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true,
 });
+
+function myFunction() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
